c768fc9e80453e6a593b2e1cceaf624ae1174aae
Anze Liu <anzeliu0604@berkeley.edu>
2021-07-21 20:57:44 -0700

    codebase

--- museum/urls.py
+++ museum/urls.py
@@ -3,8 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.indexP4, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('', views.index, name='index'),
     path('home', views.home, name='home'),
     path('main_hall', views.main_hall, name='main_hall'),
     path('themed_room_v1', views.themed_room_v1, name='themed_room_v1'),
---
+++ museum/views.py
@@ -0,0 +1,32 @@
+# chat/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'museum/index.html')
+
+def home(request):
+    return render(request, 'museum/home.html')
+
+def chat_room(request):
+    return render(request, 'museum/chat_room.html')
+
+def color_picker(request):
+    return render(request, 'museum/color_picker.html')
+
+def drawing_pad(request):
+    return render(request, 'museum/drawing_pad.html')
+
+def main_hall(request):
+    return render(request, 'museum/main_hall.html')
+
+def museum_display(request):
+    return render(request, 'museum/museum_display.html')
+
+def themed_room_v1(request):
+    return render(request, 'museum/themed_room_v1.html')
+
+def themed_room_v2(request):
+    return render(request, 'museum/themed_room_v2.html')
+
+def upload_artwork(request):
+    return render(request, 'museum/upload_artwork.html')
